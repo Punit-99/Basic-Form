65d6e30ce1efd7574cbd1846d845dd4d28c61e50
Punit Soni <sonipunit006@gmail.com>
2024-06-26 15:47:01 +0530

    working

--- models/User.js
+++ models/User.js
@@ -1,4 +1,4 @@
-// models/User.js
+// schema for mongodb
 
 const mongoose = require('mongoose');
 const bcrypt = require('bcryptjs');
@@ -8,22 +8,16 @@ const userSchema = new mongoose.Schema({
   password: { type: String, required: true },
 });
 
-// Hash password before saving to database
 userSchema.pre('save', async function(next) {
-  try {
-    if (!this.isModified('password')) {
-      return next();
-    }
-
-    const salt = await bcrypt.genSalt(10);
-    this.password = await bcrypt.hash(this.password, salt);
-    next();
-  } catch (error) {
-    next(error);
+  if (!this.isModified('password')) {
+    return next();
   }
+
+  const salt = await bcrypt.genSalt(10);
+  this.password = await bcrypt.hash(this.password, salt);
+  next();
 });
 
-// Method to compare passwords
 userSchema.methods.comparePassword = async function(candidatePassword) {
   return await bcrypt.compare(candidatePassword, this.password);
 };
--- server.js
+++ server.js
@@ -1,20 +1,20 @@
+// starting of backend (main file)
 const express = require("express");
 const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
 const dotenv = require("dotenv");
-const authRoutes = require("./routes/auth"); // Import auth routes
 const path = require("path");
+const authRoutes = require("./routes/auth");
 
 dotenv.config();
 
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 5000;
 
-// Middleware
 app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, "public")));
 
-// Connect to MongoDB
 mongoose
   .connect(process.env.MONGO_URI, {
     useNewUrlParser: true,
@@ -23,15 +23,16 @@ mongoose
   .then(() => console.log("MongoDB connected"))
   .catch((err) => console.error("MongoDB connection error:", err));
 
-// Routes
-app.use("/api/auth", authRoutes); // Use auth routes
-
-// Route to serve login.html at root URL ("/")
 app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "public", "login.html"));
 });
 
-// Start the server
+app.use("/api/auth", authRoutes);
+
+app.get("/dashboard", (req, res) => {
+  res.sendFile(path.join(__dirname, "public", "dashboard.html"));
+});
+
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
 });
